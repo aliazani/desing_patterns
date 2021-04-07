a0e5518f8d4b92c06f8a1d2b9881d4c0162b905f
Mohammadali Azani <aliazn2013@yahoo.com>
2021-04-07 17:05:57 +0430

    remove needless imports

--- Creational/src/Factory/Factory3/ShapeFactory.java
+++ Creational/src/Factory/Factory3/ShapeFactory.java
@@ -1,9 +1,5 @@
 package Factory.Factory3;
 
-import Factory.Factory3.Circle;
-import Factory.Factory3.Rectangle;
-import Factory.Factory3.Square;
-
 public class ShapeFactory {
     public static Shape getShape(String shapeType) {
         if (shapeType.equalsIgnoreCase("CIRCLE"))
