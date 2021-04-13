a8f769c7ff9383519c8141cc58e11c7015ac32a7
Mohammadali Azani <aliazn2013@yahoo.com>
2021-04-13 17:11:14 +0430

    Challenge for singleton design pattern that solve with Bill Pugh solution

---
+++ Creational/src/Singleton/Challenge/CaptainSingleton.java
@@ -0,0 +1,14 @@
+package Singleton.Challenge;
+
+public class CaptainSingleton {
+    private CaptainSingleton() {
+    }
+
+    private static class CaptainCreationHelper {
+        private static final CaptainSingleton uniqueCaptain = new CaptainSingleton();
+    }
+
+    public static CaptainSingleton getCaptain() {
+        return CaptainCreationHelper.uniqueCaptain;
+    }
+}
